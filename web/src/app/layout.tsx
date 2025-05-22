3373b51be5faba981f949192279c0c1e17b10e5a
Ivan Borisov <2622610@gmail.com>
2025-05-22 21:42:01 +0300

    fix init webapp

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
